e72982b663c90b652bb2b277637fc700abf476bc
pmoieni <62774242+pmoieni@users.noreply.github.com>
2025-11-28 10:30:16 +0330

    formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,86 +1,80 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import Progress from "./lib/components/Progress.svelte";
+	import { onMount } from 'svelte';
+	import Progress from './lib/components/Progress.svelte';
 
-    let isCorrupt = $state(false);
+	let isCorrupt = $state(false);
 
-    const urlParams = new URLSearchParams(window.location.search);
+	const urlParams = new URLSearchParams(window.location.search);
 
-    if (
-        !urlParams.has("title") ||
-        !urlParams.has("target") ||
-        !urlParams.has("created_at")
-    ) {
-        isCorrupt = true;
-    }
+	if (!urlParams.has('title') || !urlParams.has('target') || !urlParams.has('created_at')) {
+		isCorrupt = true;
+	}
 
-    const title = atob(urlParams.get("title")!);
-    const target = parseInt(urlParams.get("target")!);
-    const createdAt = parseInt(urlParams.get("created_at")!);
-    const totalTime = target - createdAt;
-    const now = new Date().getTime();
+	const title = atob(urlParams.get('title')!);
+	const target = parseInt(urlParams.get('target')!);
+	const createdAt = parseInt(urlParams.get('created_at')!);
+	const totalTime = target - createdAt;
+	const now = new Date().getTime();
 
-    let timeLeft = $state(target - now);
-    const timeLeftFormatted = $derived(
-        new Date(timeLeft).toISOString().substring(11, 19),
-    );
+	let timeLeft = $state(target - now);
+	const timeLeftFormatted = $derived(new Date(timeLeft).toISOString().substring(11, 19));
 
-    const getProgress = () => {
-        const now = new Date().getTime();
-        return Math.ceil(((now - createdAt) / totalTime) * 100);
-    };
+	const getProgress = () => {
+		const now = new Date().getTime();
+		return Math.ceil(((now - createdAt) / totalTime) * 100);
+	};
 
-    let progress = $state(getProgress());
-    // url: http://localhost:5173/?title=QUJJIEFieXNzIEZpbmFscw==&target=1763246124000&created_at=1763238924000
+	let progress = $state(getProgress());
+	// url: http://localhost:5173/?title=QUJJIEFieXNzIEZpbmFscw==&target=1763246124000&created_at=1763238924000
 
-    onMount(() => {
-        setInterval(() => {
-            const now = new Date().getTime();
-            timeLeft = target - now;
-            progress = getProgress();
-        }, 1000);
-    });
+	onMount(() => {
+		setInterval(() => {
+			const now = new Date().getTime();
+			timeLeft = target - now;
+			progress = getProgress();
+		}, 1000);
+	});
 </script>
 
 <main>
-    <h1>{isCorrupt ? "" : title}</h1>
-    <div class="display">
-        <img src="/glorpass.gif" alt="glorpass" />
-        <h2>{isCorrupt ? "corrupt link" : timeLeftFormatted}</h2>
-        <img src="/glorpass.gif" alt="glorpass" />
-    </div>
-    <Progress {progress} />
+	<h1>{isCorrupt ? '' : title}</h1>
+	<div class="display">
+		<img src="/glorpass.gif" alt="glorpass" />
+		<h2>{isCorrupt ? 'corrupt link' : timeLeftFormatted}</h2>
+		<img src="/glorpass.gif" alt="glorpass" />
+	</div>
+	<Progress {progress} />
 </main>
 
 <style>
-    main {
-        width: 100%;
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        background-color: #000;
+	main {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background-color: #000;
 
-        & > h1 {
-            color: #fff;
-            font-weight: bold;
-        }
+		& > h1 {
+			color: #fff;
+			font-weight: bold;
+		}
 
-        .display {
-            display: flex;
-            align-items: center;
-            justify-content: center;
+		.display {
+			display: flex;
+			align-items: center;
+			justify-content: center;
 
-            h2 {
-                color: #00ff00;
-                font-weight: bold;
-            }
+			h2 {
+				color: #00ff00;
+				font-weight: bold;
+			}
 
-            img {
-                height: 5rem;
-                width: auto;
-            }
-        }
-    }
+			img {
+				height: 5rem;
+				width: auto;
+			}
+		}
+	}
 </style>
--- src/lib/components/Progress.svelte
+++ src/lib/components/Progress.svelte
@@ -1,43 +1,43 @@
 <script lang="ts">
-    interface Props {
-        progress: number;
-    }
+	interface Props {
+		progress: number;
+	}
 
-    let { progress }: Props = $props();
+	let { progress }: Props = $props();
 </script>
 
 <div class="con">
-    <div class="progress" style={`width: ${progress}%;`}>
-        <img src="/glorp.png" alt="glorp" />
-    </div>
+	<div class="progress" style={`width: ${progress}%;`}>
+		<img src="/glorp.png" alt="glorp" />
+	</div>
 </div>
 
 <style>
-    .con {
-        height: 5rem;
-        width: 100%;
-        max-width: 40rem;
-        display: flex;
-        align-items: center;
-        justify-content: start;
-        margin-top: 2rem;
+	.con {
+		height: 5rem;
+		width: calc(100% - 10rem);
+		max-width: 40rem;
+		display: flex;
+		align-items: center;
+		justify-content: start;
+		margin-top: 2rem;
 
-        .progress {
-            height: 100%;
-            position: relative;
-            background-image: url("/rainbow.png");
-            background-position: center;
-            background-repeat: repeat-x;
-            background-size: contain;
+		.progress {
+			height: 100%;
+			position: relative;
+			background-image: url('/rainbow.png');
+			background-position: center;
+			background-repeat: repeat-x;
+			background-size: contain;
 
-            img {
-                position: absolute;
-                height: 5rem;
-                width: auto;
-                top: 50%;
-                transform: translate(-50%, -50%);
-                right: -5rem;
-            }
-        }
-    }
+			img {
+				position: absolute;
+				height: 5rem;
+				width: auto;
+				top: 50%;
+				transform: translate(-50%, -50%);
+				right: -5rem;
+			}
+		}
+	}
 </style>
